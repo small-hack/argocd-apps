b524c509326bd5c4a01ac3185fe7dbaeed721249
jessebot <jessebot@linux.com>
2024-12-15 17:44:11 +0100

    remove more ./ from scripts

--- scripts/autoupdate-prometheus-crds.sh
+++ scripts/autoupdate-prometheus-crds.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 new_version="$1"
 
-version=$(grep "targetRevision:" "./prometheus/crds/prometheus_crds_argocd_app.yaml" | awk '{print $2}' | tr -d "kube-prometheus-stack-")
+version=$(grep "targetRevision:" "prometheus/crds/prometheus_crds_argocd_app.yaml" | awk '{print $2}' | tr -d "kube-prometheus-stack-")
 
 # this shouldn't happen
 if [[ ! $version ]]; then
@@ -14,4 +14,4 @@ if [[ ! $version ]]; then
 fi
 
 echo "Bumping version for promtheus CRDs from $version to $new_version to be in line with the helm chart version"
-sed -i "s/${version}/${new_version}/" "./prometheus/crds/prometheus_crds_argocd_app.yaml"
+sed -i "s/${version}/${new_version}/" "prometheus/crds/prometheus_crds_argocd_app.yaml"
